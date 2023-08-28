4d8bcb94028cec9edd69dc298fcfc7c504cb6776
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-08-28 12:23:39 -0700

    Added context

---
+++ src/context.jsx
@@ -0,0 +1,8 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+const AppContext = createContext();
+export const AppProvider = ({ children }) => {
+	return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+};
+
+export const useGlobalContext = () => useContext(AppContext);
