0bdefb89d0d037eb02aa83841d769d9e6769a730
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-08-29 22:16:37 -0700

    Added form, logged input

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,4 +1,25 @@
 const SearchForm = () => {
-	return <h2>SearchForm</h2>;
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const searchValue = e.target.elements.search.value;
+		if (!searchValue) return;
+	};
+
+	return (
+		<section>
+			<h1 className="title">Unsplash Images</h1>
+			<form className="search-form" onSubmit={handleSubmit}>
+				<input
+					type="text"
+					name="search"
+					placeholder="cat"
+					className="form-input search-input"
+				/>
+				<button className="btn" type="submit">
+					search
+				</button>
+			</form>
+		</section>
+	);
 };
 export default SearchForm;
